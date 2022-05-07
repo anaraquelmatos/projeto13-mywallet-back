e53c0835c389983e8ed67d3d34da6cfe703e194f
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-05-07 03:03:02 -0300

    POST /sign-up

---
+++ index.js
@@ -0,0 +1,67 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import joi from "joi";
+import bcrypt from "bcrypt";
+
+const app = express();
+app.use(cors());
+app.use(json());
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+let db = null;
+mongoClient.connect(() => {
+    db = mongoClient.db(process.env.DATABASE);
+});
+
+app.post("/sign-up", async (req, res) => {
+    const { name, email, password, passwordConfirmation } = req.body;
+    const userData = {
+        name,
+        email,
+        password,
+        passwordConfirmation
+    }
+    const userSchema = joi.object({
+        name: joi.string().required(),
+        email: joi.string().email().required().email(),
+        password: joi.string().alphanum().min(6).max(10).required(),
+        passwordConfirmation: joi.string().alphanum().min(6).max(10).required()
+    });
+
+    const { error } = userSchema.validateAsync(userData, { abortEarly: false });
+
+    if (error) {
+        res.status(422).send(error.details.map(detail => detail.message));
+        return;
+    }
+
+    try {
+        const passwordHash = bcrypt.hashSync(password, 10)
+        const userValidation = await db.collection("users").findOne({ email });
+        if (userValidation) {
+            res.sendStatus(409);
+            return;
+        }
+
+        if ((password !== passwordConfirmation)) {
+            res.sendStatus(422);
+            return;
+        } else {
+            delete userData.passwordConfirmation;
+        }
+
+        await db.collection("users").insertOne({ ...userData, password: passwordHash });
+        res.sendStatus(201);
+
+    }
+    catch {
+        res.sendStatus(500);
+    }
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
